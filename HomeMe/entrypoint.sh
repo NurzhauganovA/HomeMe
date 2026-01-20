02c4053aa8b0f49fce9b7bdc481f1c11431f3102
a.nurzhauganov@ggloberce.capital <a.nurzhauganov@ggloberce.capital>
2026-01-20 16:08:38 +0500

    fixes

--- HomeMe/entrypoint.sh
+++ HomeMe/entrypoint.sh
@@ -3,10 +3,20 @@
 # Остановка при любой ошибке
 set -e
 
+echo "Waiting for postgres..."
+# Ждем пока БД (хост 'db') станет доступна на порту 5432
+while ! nc -z db 5432; do
+  sleep 0.5
+done
+echo "PostgreSQL started"
+
+# Накатываем миграции
 echo "Running migrations..."
 python manage.py migrate
 
+# Собираем статику (для админки и дашборда)
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Запускаем команду, переданную в аргументах (или gunicorn по умолчанию)
 exec "$@"
