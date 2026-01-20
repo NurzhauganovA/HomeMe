eb881a4e8fc741e404802db7ecc27f4cbd730122
a.nurzhauganov@ggloberce.capital <a.nurzhauganov@ggloberce.capital>
2026-01-20 16:21:17 +0500

    fixes

--- HomeMe/telegram_bot/apps.py
+++ HomeMe/telegram_bot/apps.py
@@ -5,5 +5,5 @@ class TelegramBotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'telegram_bot'
 
-    def ready(self):
-        import telegram_bot.signals  # noqa
+    # def ready(self):
+    #     import telegram_bot.signals  # noqa
