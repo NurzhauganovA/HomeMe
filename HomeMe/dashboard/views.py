389dd9e7125215010b25b13965b9d6f9f0e11c18
a.nurzhauganov@ggloberce.capital <a.nurzhauganov@ggloberce.capital>
2026-05-25 17:07:02 +0500

    fixes

--- HomeMe/dashboard/views.py
+++ HomeMe/dashboard/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DeleteView, View, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.shortcuts import redirect, get_object_or_404
